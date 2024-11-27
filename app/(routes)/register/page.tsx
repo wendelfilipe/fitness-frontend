3baf238a3926787d38552d4161af01e00bc946d9
wendelfilipe <wendelfilipe1997@hotmail.com>
2024-11-26 23:53:33 -0300

    create route

---
+++ app/(routes)/register/page.tsx
@@ -0,0 +1,14 @@
+export default function Register(){
+    return (
+        <div className="container bg-white flex items-center">
+            <div className="bg-black ">
+                <label htmlFor="Name">Nome</label>
+                <input type="text" id="Name" name="Name"/>
+            </div>
+            <div>
+                <label htmlFor="Name">Nome</label>
+                <input type="text" id="Name" name="Name"/>
+            </div>
+        </div>
+    )
+}
