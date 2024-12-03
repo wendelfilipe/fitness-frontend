a93f7eb3cda141696524a8bc89a6118152de019b
wendelfilipe <wendelfilipe1997@hotmail.com>
2024-12-02 22:11:02 -0300

    create register page

--- app/(routes)/register/page.tsx
+++ app/(routes)/register/page.tsx
@@ -1,14 +1,33 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import React from "react";
+
 export default function Register(){
+    function handleClick(e: React.FormEvent){
+        e.preventDefault();
+        console.log("Clickou")
+    }
+    
     return (
-        <div className="container bg-white flex items-center">
-            <div className="bg-black ">
-                <label htmlFor="Name">Nome</label>
-                <input type="text" id="Name" name="Name"/>
-            </div>
-            <div>
-                <label htmlFor="Name">Nome</label>
-                <input type="text" id="Name" name="Name"/>
-            </div>
+        <div className="container bg-white flex flex-col justify-center items-center h-screen">
+            <form className="bg-white shadow-2xl p-8 rounded-2xl">
+                <div className="flex flex-col w-96">
+                    <label htmlFor="Name">Nome</label>
+                    <input className="border-black border-2 rounded-md p-2" type="text" id="Name" name="Name" />
+                </div>
+                <div className="flex flex-col w-96 mt-3">
+                    <label htmlFor="Email">Senha</label>
+                    <input className="border-black border-2 rounded-md p-2" type="text" id="Email" name="Email" />
+                </div>
+                <div className="flex flex-col w-96 mt-3">
+                    <label htmlFor="Email">Confirmar Senha</label>
+                    <input className="border-black border-2 rounded-md p-2" type="text" id="Email" name="Email" />
+                </div>
+                <div className="mt-3 flex flex-col items-center">
+                    <Button onClick={(e: React.FormEvent) => handleClick(e)}>Click</Button>
+                </div>
+            </form>
         </div>
     )
 }
