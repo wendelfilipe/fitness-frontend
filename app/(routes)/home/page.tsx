362af25bea93dbbf0debd1b684d3d4a3952d3526
wendelfilipe <wendelfilipe1997@hotmail.com>
2024-12-04 23:15:42 -0300

    create login page

---
+++ app/(routes)/home/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Home(){
+    return(
+        <div className="h-screen bg-gray-400 m-5">
+            <h3 className="flex justify-center items-center">Home page</h3>
+        </div>
+    )
+}
--- app/(routes)/register/page.tsx
+++ app/(routes)/register/page.tsx
@@ -1,31 +1,38 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import Link from "next/link";
 import React from "react";
 
 export default function Register(){
-    function handleClick(e: React.FormEvent){
-        e.preventDefault();
-        console.log("Clickou")
+    function handleClick(){
+        
     }
     
     return (
         <div className="container bg-white flex flex-col justify-center items-center h-screen">
-            <form className="bg-white shadow-2xl p-8 rounded-2xl">
+            <form className="bg-white shadow-2xl shadow-red-600 p-8 rounded-2xl">
                 <div className="flex flex-col w-96">
-                    <label htmlFor="Name">Nome</label>
-                    <input className="border-black border-2 rounded-md p-2" type="text" id="Name" name="Name" />
+                    <label htmlFor="Name">Name</label>
+                    <input className="border-black border-2 rounded-md p-2 my-1" type="text" id="Name" name="Name" placeholder="Name"/>
                 </div>
                 <div className="flex flex-col w-96 mt-3">
-                    <label htmlFor="Email">Senha</label>
-                    <input className="border-black border-2 rounded-md p-2" type="text" id="Email" name="Email" />
+                    <label htmlFor="Email">Password</label>
+                    <input className="border-black border-2 rounded-md p-2 my-1" type="password" id="Email" name="Email" placeholder="Password" />
                 </div>
                 <div className="flex flex-col w-96 mt-3">
-                    <label htmlFor="Email">Confirmar Senha</label>
-                    <input className="border-black border-2 rounded-md p-2" type="text" id="Email" name="Email" />
+                    <label htmlFor="Email">Confirm Password</label>
+                    <input className="border-black border-2 rounded-md p-2 my-1" type="password" id="Email" name="Email" placeholder="Confirm password"/>
                 </div>
-                <div className="mt-3 flex flex-col items-center">
-                    <Button onClick={(e: React.FormEvent) => handleClick(e)}>Click</Button>
+                <div className="mt-3 flex flex-row justify-center">
+                    <div className="mx-5">
+                        <Button onClick={() => handleClick()}>Register</Button>
+                    </div>
+                    <div className="mx-5">
+                        <Link href="/">
+                            <Button className="px-7" onClick={() => handleClick()}>Back</Button>
+                        </Link>
+                    </div>
                 </div>
             </form>
         </div>
